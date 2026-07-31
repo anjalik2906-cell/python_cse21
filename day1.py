9f55e33b461cd900fdc44af0f5dfdc5bd78f8a29
Anjali <anjalik.2906@gmail.com>
2026-07-31 09:14:41 +0530

    python

---
+++ day1.py
@@ -0,0 +1,19 @@
+x="Hello World"
+print(x)
+#This is a simple comment
+#sets in python
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+fruits = {"apple", "banana", "cherry", "apple"}
+del fruits
+A={1,2,3,4,5}
+B={4,5,6,7,8}
+print(A|B)
+print(A&B)
+thisdict ={
+    "brand": "Ford",
+    "electric" : False,
+    "year" : 1964,
+    "colors" : ["red", "white", "blue"]
+}
+print(thisdict)
